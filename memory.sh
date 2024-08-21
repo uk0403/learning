81e44226ff932dd8ddbfbb389cc828821d32bf82
uk0403 <uppili.official@gmail.com>
2024-08-21 13:08:23 +0530

    memory check file

---
+++ memory.sh
@@ -0,0 +1,10 @@
+# This is the script file 
+# It will provide the free space and memory output 5 seconds interval till user should interupt the script
+
+#! /bin/bash
+
+while true
+do
+	free -h
+	sleep 5
+done
